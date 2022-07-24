c5e47fed1de36f94d4fee3e0a156938b7770178d
Rexbas <rexbasie@gmail.com>
2022-07-24 16:09:14 +0200

    Update colored balls

--- src/main/java/com/rexbas/bouncingballs/datagen/BouncingBallsItemModelProvider.java
+++ src/main/java/com/rexbas/bouncingballs/datagen/BouncingBallsItemModelProvider.java
@@ -1,7 +1,7 @@
 package com.rexbas.bouncingballs.datagen;
 
 import com.rexbas.bouncingballs.BouncingBalls;
-import com.rexbas.bouncingballs.BouncingBallsAPI;
+import com.rexbas.bouncingballs.api.BouncingBallsAPI;
 import com.rexbas.bouncingballs.init.BouncingBallsItems;
 
 import net.minecraft.data.DataGenerator;
@@ -22,7 +22,14 @@ public class BouncingBallsItemModelProvider extends ItemModelProvider {
 	protected void registerModels() {
 		BouncingBallsItems.ITEMS.getEntries().forEach((ball) -> {
 			String name = ball.getId().getPath();
-			this.getBuilder(name).parent(bouncingball).texture("layer0", "item/" + name);
+			this.getBuilder(name).parent(bouncingball)
+			.texture("north", "item/" + name)
+			.texture("east", "item/" + name)
+			.texture("south", "item/" + name)
+			.texture("west", "item/" + name)
+			.texture("up", "item/" + name)
+			.texture("down", "item/" + name)
+			.texture("handle", "item/" + name + "_handle");
 		});
 	}
 }
--- src/main/java/com/rexbas/bouncingballs/init/BouncingBallsItems.java
+++ src/main/java/com/rexbas/bouncingballs/init/BouncingBallsItems.java
@@ -2,14 +2,8 @@ package com.rexbas.bouncingballs.init;
 
 import com.rexbas.bouncingballs.BouncingBalls;
 import com.rexbas.bouncingballs.api.item.BouncingBall;
-import com.rexbas.bouncingballs.item.DynamiteBouncingBall;
-import com.rexbas.bouncingballs.item.EggBouncingBall;
-import com.rexbas.bouncingballs.item.ObsidianBouncingBall;
-import com.rexbas.bouncingballs.item.SlimeBouncingBall;
-import com.rexbas.bouncingballs.item.SnowBouncingBall;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,24 +14,24 @@ public class BouncingBallsItems {
 	
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BouncingBalls.MODID);
 	
-	public static final RegistryObject<Item> BLACK = ITEMS.register("black", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> RED = ITEMS.register("red", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> DARKGREEN = ITEMS.register("darkgreen", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> BROWN = ITEMS.register("brown", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> BLUE = ITEMS.register("blue", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> PURPLE = ITEMS.register("purple", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> CYAN = ITEMS.register("cyan", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> LIGHTGRAY = ITEMS.register("lightgray", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> GRAY = ITEMS.register("gray", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> PINK = ITEMS.register("pink", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> GREEN = ITEMS.register("green", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> YELLOW = ITEMS.register("yellow", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> LIGHTBLUE = ITEMS.register("lightblue", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> MAGENTA = ITEMS.register("magenta", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
-	public static final RegistryObject<Item> ORANGE = ITEMS.register("orange", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
 	public static final RegistryObject<Item> WHITE = ITEMS.register("white", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> ORANGE = ITEMS.register("orange", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> MAGENTA = ITEMS.register("magenta", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> LIGHTBLUE = ITEMS.register("lightblue", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> YELLOW = ITEMS.register("yellow", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> LIME = ITEMS.register("lime", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> PINK = ITEMS.register("pink", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> GRAY = ITEMS.register("gray", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> LIGHTGRAY = ITEMS.register("lightgray", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> CYAN = ITEMS.register("cyan", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> PURPLE = ITEMS.register("purple", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> BLUE = ITEMS.register("blue", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> BROWN = ITEMS.register("brown", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> GREEN = ITEMS.register("green", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> RED = ITEMS.register("red", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
+	public static final RegistryObject<Item> BLACK = ITEMS.register("black", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties()));
 
-	public static final RegistryObject<Item> EGG = ITEMS.register("egg", () -> new EggBouncingBall());
+	/*public static final RegistryObject<Item> EGG = ITEMS.register("egg", () -> new EggBouncingBall());
 	public static final RegistryObject<Item> SNOW = ITEMS.register("snow", () -> new SnowBouncingBall());
 	public static final RegistryObject<Item> DYNAMITE = ITEMS.register("dynamite", () -> new DynamiteBouncingBall());
 	public static final RegistryObject<Item> SLIME = ITEMS.register("slime", () -> new SlimeBouncingBall());
@@ -50,5 +44,5 @@ public class BouncingBallsItems {
 	public static final RegistryObject<Item> OBSIDIAN = ITEMS.register("obsidian", () -> new ObsidianBouncingBall());
 	public static final RegistryObject<Item> QUARTZ = ITEMS.register("quartz", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties(550, Items.QUARTZ, 1.0f, 0.8f, 10f, 0.2f)));
 	public static final RegistryObject<Item> EMERALD = ITEMS.register("emerald", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties(600, Items.EMERALD, 1.25f, 1.5f, 16f, 0.1f)));
-	public static final RegistryObject<Item> NETHERSTAR = ITEMS.register("netherstar", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties(2500, Items.NETHER_STAR, 1.75f, 1.5f, 18f, 0f)));
+	public static final RegistryObject<Item> NETHERSTAR = ITEMS.register("netherstar", () -> new BouncingBall(new Item.Properties().tab(BouncingBalls.ITEMGROUP), new BouncingBall.Properties(2500, Items.NETHER_STAR, 1.75f, 1.5f, 18f, 0f)));*/
 }
